4fee14c8364b08e49a7bd7232d306d9fc82e1804
Cole Price <colevart@gmail.com>
2022-11-04 14:42:14 -0400

    init

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+Song.create(title: "Who let the dogs out", album: "Who let the dogs out", artist: "Baha Men", year: 2000)
+
+Song.create(title: "I get knocked down", album: "Tubthumper", artist: "Chumbawamba", year: 1997)
+
+Song.create(title: "Blue (Da Ba Dee)", album: "Blue", artist: "Eiffel 65", year: 1998)
+
+Song.create(title: "Oops! I did it again", album: "Oops! I did it again", artist: "Britney Spears", year: 2000)
