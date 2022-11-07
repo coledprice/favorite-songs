8d6345ba363719c0d9539a3ad7a4df9114f9d0e4
Cole Price <colevart@gmail.com>
2022-11-07 10:07:41 -0500

    creates five restful actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/songs" => "songs#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post "/songs" => "songs#create"
+
+  get "/songs/:id" => "songs#show"
+
+  patch "/songs/:id" => "songs#update"
+
+  delete "/songs/:id" => "songs#destroy"
 end
